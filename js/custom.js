a78d40ed3466e922a145d386b28b3a8ca038bbf8
abdullahh2 <warexxplus@gmail.com>
2020-11-15 16:49:09 +0300

    Timer ve teste başla butonu eklendi

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,30 @@
 $(function() {
-
+    $("#startTest").on("click", () => {
+        $("#startTest").hide();
+        $(".tests").show();
+        startTimer();
+    });
 });
+
+var startTimer = () => {
+    var dk = 9;
+    var sn = 59;
+    setInterval(
+        () => {
+            $("#timer").html("0" + dk + ":" + (sn.toString().length == 1 ? "0" + sn : sn));
+            sn -= 1;
+            if (sn == 0 && dk != 0) {
+                dk -= 1;
+                sn = 59;
+            }
+            if (dk == 0 && sn == 0) {
+                testFinished();
+            }
+        },
+        1000
+    );
+}
+
+var testFinished = () => {
+    location.reload();
+}
